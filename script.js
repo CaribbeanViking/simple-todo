e7c43a012ba681e0bd873f218c9dda14598055ca
CaribbeanViking <emil.w.nyfelt@gmail.com>
2021-04-23 16:44:05 -0400

    Made HTML and style

---
+++ script.js
@@ -0,0 +1,9 @@
+const addBtnEl = document.getElementById("addBtn");
+const inputEl = document.getElementById("input");
+const ulEl = document.getElementById("ul")
+
+addBtnEl.addEventListener("click", () => {
+ const liEl = document.createElement("li");
+ ulEl.appendChild("li");
+
+})
