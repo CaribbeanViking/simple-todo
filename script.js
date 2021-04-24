7d8225c4a2a89c7d125b2c347eede842e7a6a43b
CaribbeanViking <emil.w.nyfelt@gmail.com>
2021-04-23 20:53:02 -0400

    Added functionality

--- script.js
+++ script.js
@@ -2,8 +2,34 @@ const addBtnEl = document.getElementById("addBtn");
 const inputEl = document.getElementById("input");
 const ulEl = document.getElementById("ul")
 
-addBtnEl.addEventListener("click", () => {
- const liEl = document.createElement("li");
- ulEl.appendChild("li");
+inputEl.addEventListener("keypress", (e) => {
+ if (e.key === "Enter") {
+  addElement();
+ }
+});
 
-})
+addBtnEl.addEventListener("click", () => {
+ addElement();
+});
+
+function addElement() {
+ if (inputEl.value) {
+  const liEl = document.createElement("li");
+  liEl.addEventListener("click", () => {
+   liEl.classList.toggle("done");
+  });
+
+  liEl.innerText = inputEl.value;
+  inputEl.value = "";
+  const btnEl = document.createElement("button");
+  btnEl.innerText = "X";
+  btnEl.className = "del-btn";
+  btnEl.addEventListener("click", () => {
+   liEl.remove();
+  });
+  liEl.appendChild(btnEl)
+  ulEl.appendChild(liEl);
+ } else {
+  alert("You must write something first!");
+ }
+};
